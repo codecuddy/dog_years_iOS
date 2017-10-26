d57641f7764b1a2a893448b2df3614c7d393bc65
Charlie <charliecuddy4@gmail.com>
2017-10-25 23:32:26 -0500

    dog years

--- dog years/ViewController.swift
+++ dog years/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
+    @IBOutlet weak var yearsInputTextField: UITextField!
+    
+    @IBAction func calculateButtonClicked(_ sender: Any) {
+        print("clicked it")
+        
+        if let yearsOld = yearsInputTextField.text {
+            if let yearsOldInt = Int(yearsOld) {
+                let dogYears = yearsOldInt * 7
+            
+                dogYearsLabel.text = "Your dog is " + String(dogYears) + " in dog years."
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
